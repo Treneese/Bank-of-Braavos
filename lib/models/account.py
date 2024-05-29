71e1d7097c43b40c32cfeb883b011f5c02bf59f8
treneese <treneesejohnson9012@gmail.com>
2024-05-29 13:49:24 -0400

    branch update sql start

--- lib/models/account.py
+++ lib/models/account.py
@@ -12,6 +12,7 @@ class Account:
         self.account_type = account_type
         self.balance = balance
         self.payment_history = payment_history
+        Account.all[account_number] = self
 
     @property
     def account_number(self):
@@ -22,11 +23,8 @@ class Account:
         if isinstance(account_number, int) and 12 <= len(str(account_number)) <= 17:
             self._account_number = account_number
         else:
-            raise ValueError(
-                "Account Number must be an Integer between 12 and 17 digits"
-            )
+            raise ValueError("Account Number must be an integer between 12 and 17 digits")
 
-        
     @property
     def routing_number(self):
         return self._routing_number
@@ -36,10 +34,8 @@ class Account:
         if isinstance(routing_number, int) and len(str(routing_number)) == 9:
             self._routing_number = routing_number
         else:
-            raise ValueError(
-                "Routing Number must be an Integer of 9 digits"
-            )
-        
+            raise ValueError("Routing Number must be an integer of 9 digits")
+
     @property
     def account_type(self):
         return self._account_type
@@ -49,33 +45,26 @@ class Account:
         if isinstance(account_type, str) and account_type in self.ALLOWED_ACCOUNT_TYPES:
             self._account_type = account_type
         else:
-            raise ValueError(
-                "Account Type must be (Saving, Checkings, Business or Credit)"
-            )
-        
+            raise ValueError("Account Type must be one of: 'Saving', 'Checking', 'Business', or 'Credit'")
+
     @property
     def balance(self):
         return self._balance
 
     @balance.setter
     def balance(self, balance):
-        if isinstance(balance, int):
-            self.balance= balance
+        if isinstance(balance, (int, float)):
+            self._balance = balance
         else:
-            raise ValueError(
-                "Balance must be an Interger"
-            )
-        
+            raise ValueError("Balance must be a number")
+
     @property
     def payment_history(self):
         return self._payment_history
 
     @payment_history.setter
     def payment_history(self, payment_history):
-        # Example validation: checking if payment_history is a list
         if isinstance(payment_history, list):
             self._payment_history = payment_history
         else:
-            raise ValueError(
-                "Payment History must be a list"
-            )
+            raise ValueError("Payment History must be a list")
