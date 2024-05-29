9a35c360906a5485e19cffd18a52a57b07404dab
Ibrahem Younas <younasibrahem@gmail.com>
2024-05-29 15:05:18 -0400

    updated cli menus/ helper functions

--- lib/cli.py
+++ lib/cli.py
@@ -1,8 +1,16 @@
 # lib/cli.py
-from client import Client
+from models.client import Client
 from helpers import (
     exit_program,
-    helper_1
+    add_client,
+    add_account,
+    view_accounts,
+    find_account_by_acc_number,
+    find_client_by_id,
+    view_client_accounts,
+    delete_account,
+    delete_client,
+
 )
 def create_client():
     name = input("Enter name: ")
@@ -41,18 +49,131 @@ def show_menu():
 
 def main():
     while True:
-        show_menu()
-        choice = input("Enter choice: ")
-
-        if choice == '1':
-            create_client()
-        elif choice == '2':
-            list_clients()
-        elif choice == '3':
-            print("Exiting...")
-            break
+        menu()
+        choice = input("> ")
+        if choice == "0":
+            exit_program()
+        elif choice == "1":
+            add_menu()
+        elif choice == "2":
+            view_menu()
+        elif choice == "3":
+            deposit_menu()
+        elif choice == "4":
+            delete_menu()
         else:
-            print("Invalid choice. Please try again.")
+            print("\nInvalid choice")
+
+def menu():
+    print("\nWelcome, weary travaler, to The Iron Bank of Braavos!")
+    print("Please select an option:")
+    print("0. Exit the program")
+    print("1. Add/register an account")
+    print("2. View your accounts")
+    print("3. Withdraw/deposit funds")
+    print("4. Delete account/ End your business with us")
+
+def add_menu():
+    while True:
+        create_menu_options()
+        choice = input("> ")
+        if choice == "0":
+            exit_program()
+        elif choice == "1":
+            main()
+        elif choice == "2":
+            add_client()
+            add_menu()
+        elif choice == "3":
+            add_account()
+            add_menu()
+        else:
+            print("Invalid choice")
+
+def create_menu_options():
+    print("\n-Now in Register Menu-")
+    print("Please select an option:")
+    print("0. Exit Program")
+    print("1. Return to Main Menu")
+    print("2. Register as a new Client")
+    print("3. Add an account")
+
+def view_menu():
+    while True:
+        view_menu_options()
+        choice = input("> ")
+        if choice == "0":
+            exit_program()
+        elif choice == "1":
+            main()
+        elif choice == "2":
+            find_client_by_id()
+        elif choice == "3":
+            view_client_accounts()
+        elif choice == "4":
+            find_account_by_acc_number()
+        else:
+            print("Invalid choice")
+        view_menu()
+
+def view_menu_options():
+    print("\n-Now in Viewing Menu-")
+    print("Please select an option:")
+    print("0. Exit Program")
+    print("1. Return to Main Menu")
+    print("2. Find account by id")
+    print("3. View your accounts")
+    print("4. Find account by Account #")
+    
+def deposit_menu():
+    while True:
+        deposit_menu_options()
+        choice = input("> ")
+        if choice == "0":
+            exit_program()
+        elif choice == "1":
+            main()
+        elif choice == "2":
+            deposit_account()     
+        elif choice == "3":
+            withdraw_account()
+        else :
+            print("Invalid choice")
+
+def deposit_menu_options():
+    print("\n-Now in Deposit/Withdraw Menu-")
+    print("Please select an option:")
+    print("0. Exit Program")
+    print("1. Return to Main Menu")
+    print("2. Deposit into Account")
+    print("3. Withdraw from Account")
+
+def delete_menu():
+    while True:
+        delete_menu_options()
+        choice = input("> ")
+        if choice == "0":
+            exit_program()
+        elif choice == "1":
+            main()    
+        elif choice == "2":
+            delete_account()
+        elif choice == "3":
+            delete_client()
+        else :
+            print("Invalid choice")
+
+
+def delete_menu_options():
+    print("\n-Now in Delete Menu-")
+    print("Please select an option:")
+    print("0. Exit Program")
+    print("1. Return to Main Menu")
+    print("2. Delete an account")
+    print("3. Close all accounts")
+    
+
+
 
 if __name__ == '__main__':
     main()
---
+++ lib/helpers.py
@@ -0,0 +1,107 @@
+from models.account import Account
+from models.client import Client
+from random import randint
+
+def exit_program():
+    print("Thank you for Banking with us. Goodbye!")
+    exit()
+
+def random_with_N_digits(n):
+    range_start = 10**(n-1)
+    range_end = (10**n)-1
+    return randint(range_start, range_end)
+
+def add_client():
+    try:
+        client = input("User name: ")
+        address = input("Place of residence: ")
+        dob = input("Enter date of birth in the format YYYYMMDD: ")
+        email = input("Enter email address: ")
+        income = input("Enter yearly gross income: ")
+        credit_score = input("Enter your credit score: ")
+        Client.create(client, address, dob, email, income, credit_score)
+        print(f"\nClient {Client} added sucessfully.")
+    except Exception as exc:
+        print(f"\nError: {exc}")
+
+def add_account():
+    try:
+        account_type = input(f"Choose which account type you would like to open {Account.ALLOWED_ACCOUNT_TYPES}")
+        while account_type not in Account.ALLOWED_ACCOUNT_TYPES:
+            print("Invalid Category")
+            account_type = input(f"Choose which account type you would like to open {Account.ALLOWED_ACCOUNT_TYPES}")
+        acc_num = random_with_N_digits(12)
+        routing_num = random_with_N_digits(9)
+        Account.create(acc_num, routing_num, account_type)
+        print(f"\n{account_type} account created sucessfully.")
+    except Exception as exc:
+        print(f"\nError: {exc}")
+
+
+def view_accounts():
+    accounts = Account.get_all()
+    if accounts:
+        for account in accounts:
+            print(f"\n-{account.account_type} account- Routing number: {account.routing_number}  Balance: {account.balance}")
+        else:
+            print("\nNo Accounts found.")
+
+def find_account_by_acc_number():
+    id_ = input("Enter the account number: ")
+    account = Account.find_by_id(id_)
+    if account:
+        print(f"\nAccount Found: -{account.account_number}- Type: {account.account_type}")
+    else: 
+        print(f"\nAccount {id_} not found")
+
+def find_client_by_id():
+    id_ = input("Enter the Client ID number: ")
+    client = Client.find_by_id(id_)
+    if client:
+        print(f"\nAccount Found: -{client.name}- DOB: {client.DOB}")
+    else: 
+        print(f"\nAccount {id_} not found")
+
+def view_client_accounts():
+    client_id = input("Enter the client's id: ")
+    client_object = Client.find_by_id(client_id)
+    if client_object:
+        accounts = client_object.find_all_accs()
+        if not accounts: print(f"\nClient {client_object.name} has no accounts yet")
+        try:
+            for account in accounts:
+                print(f"\nType: {account.account_type} || Balance: {account.balance} || {account.payment_history}")
+        except Exception as exc:
+            print("Error finding Account: ", exc)
+    else :
+        print(f"\nClient id of {client_id} not found")
+
+def delete_account():
+    account_ids = [account.account_number for account in Account.get_all()]
+    account_number = input(f"Enter the account number you'd like to delete from this list {account_ids}: ")
+    account = account.find_by_acc(account_number)
+    if account:
+        account.delete()
+        print(f"\naccount -{account_number}- has been deleted")
+    else :
+        print(f"\nclient -{account_number}- not found")
+
+
+def delete_client():
+    client_ids = [f"{object.name}: {object.id_number}" for object in Client.get_all()]
+    if len(client_ids) > 0:
+        client_id = input(f"To delete a client, select a client id from this list {client_ids}: ")
+        while client_id not in [str(object.id) for object in Client.get_all()]:
+            print("Client id is not in list")
+            client_id = input(f"To delete a client, select a client id from this list {client_ids}: ")
+        selected_client = Client.find_by_id(client_id)
+        print(f"\nYou deleted {selected_client.name} and all their accounts")
+        client_accounts = [account for account in Account.get_all()]
+        for account in client_accounts:
+            if str(account.client_id) == client_id:
+                account.delete()
+            else:
+                continue
+        selected_client.delete()
+    else:
+        print("\nThere are no clients to delete")
